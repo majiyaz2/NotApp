98777620108bce3deb8a5ce663c8b13882e0ade9
Andile Mhlongo <majiyaz2@gmail.com>
2024-02-15 21:23:34 +0200

    Added a dynamic port number

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
-const app = express()
+
+const app = express();
+const port = process.env.PORT || 4000;
 
 app.get('/', (req, res)=>res.send("Hello World"))
-app.listen(4000, () => console.log('listening on port 4000!'))
+app.listen(4000, () => 
+    console.log(`Server running at http://localhost:${port}`)
+);
