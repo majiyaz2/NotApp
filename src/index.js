03530a14b1561291f5c4f0db52a76c02901220a9
Andile Mhlongo <majiyaz2@gmail.com>
2024-02-15 22:15:01 +0200

    Added the Apollo server

--- src/index.js
+++ src/index.js
@@ -1,9 +1,36 @@
 const express = require('express');
+const {ApolloServer, gql} = require('apollo-server-express');
 
-const app = express();
 const port = process.env.PORT || 4000;
 
-app.get('/', (req, res)=>res.send("Hello World"))
+const typeDefs = gql`
+type Query{
+    hello: String
+}
+`;
+
+const resolvers = {
+    Query: {
+        hello: () => 'Hello world!'
+    }
+}
+
+server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+const app = express();
+async function startServer() {
+ 
+    await server.start();
+    server.applyMiddleware({app, path: '/api'});
+}
+startServer();
+
+
+
+
+// app.get('/', (req, res)=>res.send("Hello World"))
 app.listen(4000, () => 
-    console.log(`Server running at http://localhost:${port}`)
+    console.log(`Server running at http://localhost:${port}${server.graphqlPath}`)
 );
