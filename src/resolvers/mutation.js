43416656923a0f4327917ba8adc810275d7ddf21
Andile Mhlongo <majiyaz2@gmail.com>
2024-02-18 19:11:23 +0200

    Updated the schema and mutation to add a new note assigned to the user

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcrypt");
+const mongoose = require('mongoose')
 const jwt = require("jsonwebtoken");
 const gravatar = require("../util/gravatar")
 const {
@@ -8,11 +9,13 @@ const {
 require("dotenv").config()
 
 module.exports = {
-    newNote: async (parent, args, {models}) => {
-
+    newNote: async (parent, args, {models, user}) => {
+        if(!user){
+            throw new AuthenticationError('You must be signed in to create a note')
+        }
         return await models.Note.create({
             content: args.content,
-            author: 'Adam Scott'
+            author: mongoose.Types.ObjectId(user.id)
         });
     },
     deleteNote: async (parent, {id}, {models}) => {
