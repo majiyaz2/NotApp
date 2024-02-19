ef1b3aafe9ec2cbfc2afe24284da6335f28c505c
Andile Mhlongo <majiyaz2@gmail.com>
2024-02-19 21:30:22 +0200

    Added limitations to query depth and complexity of queries

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 require('dotenv').config();
-const jwt = require("jsonwebtoken")
-const helmet = require('helmet')
-const cors = require('cors')
+const jwt = require("jsonwebtoken");
+const helmet = require('helmet');
+const cors = require('cors');
+const depthLimit = require("graphql-depth-limit");
+const {createComplexityLimitRule} = require('graphql-validation-complexity');
 // Loading the dependencies
 const express = require('express');
 const {ApolloServer} = require('apollo-server-express');
 
 // Loading local modules
-const db = require('./db')
-const models = require('./models')
-const typeDefs = require('./schema')
-const resolvers = require('./resolvers')
+const db = require('./db');
+const models = require('./models');
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
 
 // Setting the port and host ip
 const port = process.env.PORT || 4000;
@@ -20,25 +22,26 @@ const DB_HOST = process.env.DB_HOST;
 const getUser = token => {
     if(token) {
         try {
-            return jwt.verify(token, process.env.JWT_SECRET)
+            return jwt.verify(token, process.env.JWT_SECRET);
         }catch(err){
-            throw new Error('Session invalid')
+            throw new Error('Session invalid');
         }
     }
 }
 
 const app = express();
-app.use(helmet())
-app.use(cors())
+app.use(helmet());
+app.use(cors());
 db.connect(DB_HOST);
 server = new ApolloServer({
     typeDefs,
     resolvers,
+    validationRules: [depthLimit(5), createComplexityLimitRule(1000)],
     context:  ({req}) => {
         const token = req.headers.authorization;
-        const user = getUser(token)
-        console.log(user)
-        return {models, user}
+        const user = getUser(token);
+        console.log(user);
+        return {models, user};
     },
     
 });
--- src/resolvers/query.js
+++ src/resolvers/query.js
@@ -9,7 +9,7 @@ module.exports = {
         return await models.User.findOne({username});
     },
     users: async (parent, args , {models}) => {
-        return await models.User.find({});
+        return await models.User.find({}).limit(100);
     },
     me: async (parent, args, {models, user}) => {
         return await models.User.findById(user.id);
